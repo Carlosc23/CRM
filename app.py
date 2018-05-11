affe0d9f5fda5b8cc90723e7ea2d411de81613c0
Carlos <carlos.calderon097@gmail.com>
2018-05-10 23:06:06 -0600

    feat: add initial gui
    It was added a simple menu Overview html for the principal view
    of the system. It also was added a simple form to insert clients

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
-from flask import Flask
+import os
 
+from flask import Flask, render_template, request
+
+import urllib3
+import sys
+import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+urllib3.disable_warnings()
+
+cloudinary.config(
+    cloud_name = 'drc2h3d9n',
+    api_key = '596916237749538',
+    api_secret = '78HRHwp-Ceo7DktRXeMh2gDNdU0'
+    )
+
+
+UPLOAD_FOLDER = 'static/tmpImages'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('main.html')
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        print("hola")
+        print(request.form["option"])
+        for i in request.form:
+            print(i, "-->", request.form[i])
+        f = request.files['file']
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+        f.save(filename)
+        print(filename)
+       # img = cloudinary.uploader.upload(UPLOAD_FOLDER+'/'+f.filename)
+       # genurl = img['url']
+       # os.remove(filename)
+       # print("URL",'--> ',genurl)
+        return render_template('add.html')
+    else:
+        return render_template('add.html')
 
 
 if __name__ == '__main__':
